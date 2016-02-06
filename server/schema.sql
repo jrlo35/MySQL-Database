066258cb1ee128f0cfc22eb1750013f6411c8df0
Jonathon Lo <jonathon.lo35@gmail.com>
2016-02-05 16:43:32 -0800

    'added tables'

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,23 @@
+drop database chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  messageID int(5), username varchar(30), message varchar(140)
 );
 
+CREATE TABLE messageroom (
+  roomID int(5), messageID int(5)
+);
+
+CREATE TABLE room (
+  roomID int(5), roomname varchar(30)
+);
+
+
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
